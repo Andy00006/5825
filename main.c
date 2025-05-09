b36de52eb5a589ad49b0f20ec21d622e0e4c0ce0
Andy00006 <andy.laloum8@gmail.com>
2025-05-09 14:48:41 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "animal.h"
+
+int main() {
+    Animal animaux[TAILLE_MAX];
+    int nb_animaux = charger_animaux(FICHIER_DATA, animaux, TAILLE_MAX);
+
+    int choix;
+    do {
+        printf("\n--- Menu du refuge ---\n");
+        printf("1. Ajouter un animal\n");
+        printf("2. Afficher les animaux\n");
+        printf("3. Rechercher un animal\n");
+        printf("4. Enregistrer et quitter\n");
+        printf("Votre choix : ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1:
+                ajouter_animal(animaux, &nb_animaux);
+                break;
+            case 2:
+                afficher_animaux(animaux, nb_animaux);
+                break;
+            case 3:
+                rechercher_animaux(animaux, nb_animaux);
+                break;
+            case 4:
+                enregistrer_animaux(FICHIER_DATA, animaux, nb_animaux);
+                printf("Données enregistrées. Au revoir !\n");
+                break;
+            default:
+                printf("Choix invalide.\n");
+        }
+    } while (choix != 4);
+
+    return 0;
+}
