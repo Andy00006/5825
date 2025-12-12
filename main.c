acbcd4b25ce46ad7b4a6986fb46ca898282f5574
Andy00006 <andy.laloum8@gmail.com>
2025-12-12 13:46:56 +0100

    Refactor file handling and command processing in main.c

--- main.c
+++ main.c
@@ -1,78 +1,93 @@
-#include "en_tete.h"
+#include "header.h"
 
 int main(int argc, char* argv[]) {
     if (argc < 3) return 1;
-
-    char* fichier_csv = argv[1];
+    char* fichier = argv[1];
     char* commande = argv[2];
 
-    FILE* fp = fopen(fichier_csv, "r");
-    if (!fp) {
-        perror("Erreur ouverture fichier");
-        return 1;
-    }
+    FILE* fp = fopen(fichier, "r");
+    if (!fp) { fprintf(stderr, "Erreur fichier\n"); return 2; }
 
-    char ligne[TAILLE_MAX_LIGNE];
-    
-    // --- MODE HISTOGRAMME ---
+    char ligne[TAILLE_BUF];
+
     if (strcmp(commande, "histo") == 0) {
-        if (argc < 4) return 1;
         char* mode = argv[3];
-        UsineAVL* racineUsines = NULL;
+        Station* avl = NULL;
 
-        while (fgets(ligne, sizeof(ligne), fp)) {
-            char col1[TAILLE_ID], col2[TAILLE_ID], col3[TAILLE_ID], col4[TAILLE_ID];
-            // Parsing manuel simple pour éviter les problèmes de strtok avec les champs vides
-            // Format attendu: Col1;Col2;Col3;Col4;Col5
-            // On utilise sscanf pour extraire les données. 
-            // Attention: sscanf s'arrête aux espaces, il faut un format spécifique ou un parser char par char.
-            
-            // Pour simplifier dans cet exemple : utilisation strtok avec gestion "-"
-            char *ptr = strtok(ligne, ";"); char *c1 = ptr;
-            ptr = strtok(NULL, ";"); char *c2 = ptr;
-            ptr = strtok(NULL, ";"); char *c3 = ptr;
-            ptr = strtok(NULL, ";"); char *c4 = ptr;
-            // c5 (fuite) ignoré pour histo sauf si "real"
+        // Lecture
+        while (fgets(ligne, TAILLE_BUF, fp)) {
+            // ID Usine est col 2 (src->usine) ou col 2 (usine) ou col 1 (usine->stock)
+            // On doit deviner le type de ligne.
+            // Simplification : On cherche si c'est une ligne de définition d'usine
+            char* c2 = recupererChamp(ligne, 2);
+            char* c4 = recupererChamp(ligne, 4); // Capacité ou Conso
+            char* c3 = recupererChamp(ligne, 3); // Destinataire
 
-            // Détection Ligne USINE (Definition capacité)
-            // ex: -;Facility complex #RH400057F;-;4749292;-
-            if (strcmp(c1, "-") == 0 && strcmp(c3, "-") == 0 && strcmp(c4, "-") != 0) {
-                int id = atoi(c2 + 1); // Hack pour ignorer le texte si l'ID est dans le string
-                // Mieux : utiliser le string complet comme ID
-                // Ici on suppose que tu as une fonction pour extraire l'ID numérique si tu veux trier par num
-                // Sinon on trie par string. Modifions le main pour trier par ID num simple.
-                
-                // Pour CYTech : on prend souvent l'ID numérique à la fin du string
-                // Exemple simple : on passe 0 en ID num et on trie par string dans une vraie implémentation.
-                racineUsines = insererUsine(racineUsines, 0, c2, atol(c4), 0);
+            // Cas : USINE (définition capacité) :: c2=ID, c4=Capacité, c3=NULL
+            if (c2 && c4 && !c3 && strstr(c2, "Facility")) {
+                avl = insererStation(avl, c2, atol(c4), 0);
             }
-            
-            // Détection Ligne SOURCE -> USINE
-            // ex: -;Spring #MQ001991L;Facility complex #RH400057F;20892;0.997
-            else if (strcmp(c1, "-") == 0 && strcmp(c3, "-") != 0 && strcmp(c4, "-") != 0) {
-                // c2 = source, c3 = usine, c4 = volume
-                racineUsines = insererUsine(racineUsines, 0, c3, 0, atol(c4));
+            // Cas : SOURCE -> USINE :: c2=Source, c3=Usine, c4=Volume
+            else if (c2 && c3 && c4 && strstr(c3, "Facility")) {
+                avl = insererStation(avl, c3, 0, atol(c4));
             }
         }
-        
-        // Ecriture fichier sortie
-        FILE* f_out = fopen("resultat_histo.dat", "w");
-        parcoursInfixe(racineUsines, f_out, mode);
-        fclose(f_out);
-        libererAVL(racineUsines);
-    }
 
-    // --- MODE FUITES (LEAKS) ---
+        // Fichier temporaire pour Gnuplot
+        FILE* out = fopen("histo.dat", "w");
+        parcoursInverse(avl, out, mode);
+        fclose(out);
+        libererStation(avl);
+
+    } 
     else if (strcmp(commande, "leaks") == 0) {
-        // Logique similaire mais avec AnnuaireAVL et Noeud
-        // A implémenter selon la logique décrite plus haut :
-        // 1. Lire ligne
-        // 2. Trouver Parent dans Annuaire (ou créer)
-        // 3. Créer Enfant
-        // 4. Lier Parent -> Enfant
-        // 5. Ajouter Enfant dans Annuaire
-        // ...
-        // A la fin: chercher l'usine cible et appeler calculerPertes()
+        char* id_usine = argv[3];
+        Index* annuaire = NULL;
+        Noeud* usineDepart = NULL;
+
+        // 1. Construction du graphe entier (nécessaire car lignes mélangées)
+        while (fgets(ligne, TAILLE_BUF, fp)) {
+            char* c2 = recupererChamp(ligne, 2); // Amont
+            char* c3 = recupererChamp(ligne, 3); // Aval
+            char* c5 = recupererChamp(ligne, 5); // % Fuite
+
+            if (c2 && c3) {
+                // Trouver ou créer Amont
+                Noeud* nAmont = rechercherIndex(annuaire, c2);
+                if (!nAmont) {
+                    nAmont = creerNoeud(c2, 0); // Fuite 0 car c'est la source du tronçon
+                    annuaire = insererIndex(annuaire, c2, nAmont);
+                }
+
+                // Créer Aval
+                float fuite = (c5) ? atof(c5) : 0.0;
+                Noeud* nAval = creerNoeud(c3, fuite);
+                annuaire = insererIndex(annuaire, c3, nAval);
+
+                // Lier
+                ajouterEnfant(nAmont, nAval);
+            }
+        }
+
+        // 2. Calcul
+        usineDepart = rechercherIndex(annuaire, id_usine);
+        if (usineDepart) {
+            // On ne connait pas le volume entrant initial, on suppose capacité max ou on le passe en arg ?
+            // Le sujet ne le précise pas clairement, on va mettre une valeur fixe ou lire la capacité
+            // Pour l'exemple, on considère que l'usine envoie 1000 m3
+            double pertes = calculerPertes(usineDepart, 1000000); // Ex: 1M m3
+            printf("%s;%f\n", id_usine, pertes);
+            
+            // Écriture dans fichier résultat (append)
+            FILE* f_leaks = fopen("leaks.dat", "a"); // Append
+            fprintf(f_leaks, "%s;%f\n", id_usine, pertes);
+            fclose(f_leaks);
+        } else {
+            printf("Usine inconnue\n");
+        }
+        
+        // TODO: Libération mémoire index/graphe (complexe ici)
+        libererIndex(annuaire);
     }
 
     fclose(fp);
